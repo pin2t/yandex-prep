15c5bc24e8b50c020b956a0a128eef8995d20bf3
pin2t <pin2t@mail.ru>
2025-10-07 11:14:57 +0300

    adjust scanner buffer size

--- e.go
+++ e.go
@@ -8,19 +8,21 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	const maxCapacity = 512*1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
 	scanner.Scan()
-	s1 := scanner.Text()
+	var s1 = scanner.Text()
 	scanner.Scan()
-	s2 := scanner.Text()
-	var chars1, chars2 [26]int
-	for _, c := range s1 {
-		chars1[byte(c)-'a']++
-	}
-	for _, c := range s2 {
-		chars2[byte(c)-'a']++
-	}
-	for i := 0; i < 26; i++ {
-		if chars1[i] != chars2[i] {
+	var s2 = scanner.Text()
+    if len(s1) != len(s2) { fmt.Println(0); return }
+	var chars1 = make(map[rune]int)
+    var chars2 = make(map[rune]int)
+	for _, c := range s1 { chars1[c]++ }
+	for _, c := range s2 { chars2[c]++ }
+    if len(chars1) != len(chars2) { fmt.Println(0); return }
+	for c, n := range chars1 {
+		if n2, found := chars2[c]; !found || n != n2 {
 			fmt.Println(0)
 			return
 		}
