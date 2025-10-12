601ce26cb4015aafe06ad4d315031d431687995d
pin2t <pin2t@mail.ru>
2025-10-12 11:24:02 +0300

    fix time limit

--- g.go
+++ g.go
@@ -1,11 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
-type state struct{ city, len int }
+import "fmt"
 
 func abs(a int) int {
 	if a < 0 {
@@ -15,43 +10,37 @@ func abs(a int) int {
 }
 
 func main() {
+	type city struct{ x, y int }
 	var n int
 	fmt.Scanln(&n)
-	cities := make([]struct{ x, y int }, 0, 1000)
+	cities := make([]city, n)
 	for i := 0; i < n; i++ {
-		var x, y int
-		fmt.Scanln(&x, &y)
-		cities = append(cities, struct{ x, y int }{x, y})
+		fmt.Scanln(&cities[i].x, &cities[i].y)
 	}
 	var k, from, to int
 	fmt.Scanln(&k)
 	fmt.Scanln(&from, &to)
 	from--
 	to--
-	seen := map[int]bool{}
-	seen[from] = true
-	closer := make([]int, n)
-	for i := 0; i < n; i++ {
-		closer[i] = i
-	}
 	distance := func(i, j int) int { return abs(cities[i].x-cities[j].x) + abs(cities[i].y-cities[j].y) }
-	sort.Slice(closer, func(i, j int) bool {
-		return distance(i, to) < distance(j, to)
-	})
-	paths := make([]state, 0, 1000000)
-	paths = append(paths, state{from, 0})
-	for i := 0; i < len(paths); i++ {
-		s := paths[i]
-		seen[s.city] = true
-		for _, city := range closer {
-			if distance(city, s.city) <= k && !seen[city] {
-				if city == to {
-					fmt.Println(s.len + 1)
-					return
-				}
-				paths = append(paths, state{city, s.len + 1})
+	var c = from
+	var len = 0
+	var path = make(map[int]bool)
+	path[c] = true
+	for c != to {
+		var m = 3000000000
+		var step = c
+		for i, _ := range cities {
+			if distance(c, i) <= k && distance(i, to) < m && !path[i] {
+				step = i
+				m = distance(i, to)
 			}
 		}
+		if step == c { break }
+		c = step
+		len++
+		path[step] = true
 	}
-	fmt.Println(-1)
+	if c != to { len = -1 }
+	fmt.Println(len)
 }
